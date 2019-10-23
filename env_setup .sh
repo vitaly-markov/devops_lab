f08765994ef26ec4e248bba6e8b28b5634bd099c
Vitaly <vitali_markau@epam.com>
2019-10-23 12:41:57 +0300

    1.1

--- env_setup .sh
+++ env_setup .sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
-# dependencies 
-sudo yum install -y libffi-devel zlib-devel bzip2-devel readline-devel \
- sqlite-devel wget curl llvm ncurses-devel openssl-devel lzma-sdk-devel redhat-rpm-config 
+# dependencies
+yyum install -y libffi-devel zlib-devel bzip2-devel readline-devel sqlite-devel wget llvm ncurses-devel openssl-devel lzma-sdk-devel epel-release redhat-rpm-config  vim wget git
 
 # pyenv
 curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
@@ -18,14 +17,11 @@ pyenv install 2.7.17
 pyenv install 3.7.4
 	
 # pyenv-virtualenv 
-git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv  
-
-yum install -y python-virtualenv python-pip python3-pip
-
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 #
 make environments
-pyenv virtualenv 2.7.17 
-pyenv virtualenv 3.7.4 
+pyenv virtualenv 2.7.17 PyEnv2.7
+pyenv virtualenv 3.7.4 PyEnv3.7
 
-virtualenv --prompt="2.7.17" venv2.7
-virtualenv --prompt="3.7.4" venv3.7
+pyenv activate PyEnv3.7
+
