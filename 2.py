c6a29f776d5ee45ad339b54a676b01b07cae7e65
Vitaly <vitali_markau@epam.com>
2019-10-24 18:39:02 +0300

    1

--- 2.py
+++ 2.py
@@ -7,8 +7,8 @@ for num in range(n):
 query_name = input()
 for keys in student_marks:
     if keys == query_name:
-        sum=0
+        sum = 0
         for i in range(len(student_marks[query_name])):
             sum = sum + float(student_marks[query_name][i])
-x = float(sum/3)
-print("%.2f"%x)
+x = float(sum / 3)
+print("%.2f" % x)
