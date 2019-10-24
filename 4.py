b1b619d772a358b26a76704370bc62aa23a4b298
Vitaly <vitali_markau@epam.com>
2019-10-24 18:42:43 +0300

    1.1

--- 4.py
+++ 4.py
@@ -1,9 +1,9 @@
 N, M = map(int, input().split())
-if  M == N*3:
+if M == N * 3:
     for i in range(1, N, 2):
         print(((M - (3 * i)) // 2) * "-" + ".|." * i + ((M - (3 * i)) // 2) * "-")
     print(((M - 7) // 2) * "-" + "WELCOME" + ((M - 7) // 2) * "-")
     for i in range(N - 2, -1, -2):
         print(((M - (3 * i)) // 2) * "-" + ".|." * i + ((M - (3 * i)) // 2) * "-")
 else:
- print("print correct size ")
+    print("print correct size")
