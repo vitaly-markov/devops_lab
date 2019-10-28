d10f9a439f4002fa6366cd06888a569d6d639b82
Vitaly <vitali_markau@epam.com>
2019-10-28 16:33:47 +0300

    1.0

--- github.py
+++ github.py
@@ -12,8 +12,9 @@ parser.add_argument('--version', action='version', version='version 1.0')
 
 args = parser.parse_args()
 
-b = requests.get('https://api.github.com/repos/' + args.user[0] +
-                 '/' + args.repo[0] + '/pulls/' + args.num[0], auth=('vvvvmmm', token))
+b = requests.get(
+    'https://api.github.com/repos/' + args.user[0] +'/' + args.repo[0] + '/pulls/' + args.num[0],
+    auth=('vitaly-markov', token))
 dd = b.json()
 if args.param[0]:
     print(b.json()[args.param[0]])
