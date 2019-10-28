f6dd93181084d1bedc98f60fc55eb309c9d43d23
Vitaly <vitali_markau@epam.com>
2019-10-28 16:25:56 +0300

    1.0

--- github.py
+++ github.py
@@ -1,7 +1,6 @@
 import requests
 import argparse
-import json
-token='fbhjb'
+token = 'fbhjb'
 parser = argparse.ArgumentParser()
 parser.add_argument('-user', nargs=1, help="write github username", required=True)
 parser.add_argument('-repo', nargs=1, help="write name of repo", required=True)
@@ -13,11 +12,13 @@ parser.add_argument('--version', action='version', version='version 1.0')
 
 args = parser.parse_args()
 
-b = requests.get('https://api.github.com/repos/' + args.user[0] + '/' + args.repo[0] + '/pulls/' + args.num[0], auth=('vvvvmmm', token))
+b = requests.get('https://api.github.com/repos/' + args.user[0] +
+                 '/' + args.repo[0] + '/pulls/' + args.num[0], auth=('vvvvmmm', token))
 dd = b.json()
 if args.param[0]:
     print(b.json()[args.param[0]])
 else:
     print('bad argument')
 if args.login:
+
     print(b.json()['user']['login'])
